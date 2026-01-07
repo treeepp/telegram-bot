be618b52c43b4b54c90a53876e1edd10092083f5
treeepp <omidhoseini086@gmail.com>
2026-01-07 12:31:27 +0330

    add bot.py

--- bot.py
+++ bot.py
@@ -1,12 +1,12 @@
+import os
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
-TOKEN = "8163275069:AAGWzifhipOOxjaXMk9mzixoIzgblgORZ7k"
+TOKEN = os.getenv("BOT_TOKEN")  # از Variables Railway گرفته می‌شود
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("ربات با موفقیت روشن شد ✅")
+    await update.message.reply_text("ربات روشن شد ✅")
 
 app = ApplicationBuilder().token(TOKEN).build()
 app.add_handler(CommandHandler("start", start))
 app.run_polling()
-
